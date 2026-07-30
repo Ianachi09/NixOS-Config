3f36d77655bc2d746ffbfc87bf6926d0b3639e6b
Ianachi09 <asyraafdaniel06@gmail.com>
2026-07-31 00:13:57 +0800

    Typo #2

--- desktop.nix
+++ desktop.nix
@@ -7,7 +7,7 @@
     user = "cinnamoroll";
    };
    default_session = {
-    command = "${pkgs.greetd-tuigreet}/bin/tuigreet --time --remember --cmd 'uwsm start hyprland-uwsm.desktop'";
+    command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd 'uwsm start hyprland-uwsm.desktop'";
     user = "greeter";
    };
   };
